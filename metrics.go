9c657dfb40f5eeec760a218ced7d0b7fcf673254
Song Gao <disxiaofei@163.com>
2025-02-24 16:23:52 +0800

    add metrics
    Signed-off-by: Song Gao <disxiaofei@163.com>

---
+++ metrics.go
@@ -0,0 +1,45 @@
+package kafka
+
+import "github.com/prometheus/client_golang/prometheus"
+
+const (
+	LblType               = "type"
+	LblRule               = "rule"
+	LblOp                 = "op"
+	LblStatus             = "status"
+	LblMessageTooLargeErr = "messageTooLarge"
+	LblReq                = "req"
+	LblRetry              = "retry"
+	LblSuccess            = "ok"
+	LblErr                = "err"
+)
+
+var (
+	KafkaWriterErrCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "kafka_client",
+		Subsystem: "writer",
+		Name:      "err_counter",
+		Help:      "counter of Kafka Client Writer IO",
+	}, []string{LblType, LblRule, LblOp})
+
+	KafkaWriterBatchCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "kafka_client",
+		Subsystem: "writer_batch",
+		Name:      "counter",
+		Help:      "counter of Kafka Client Writer IO",
+	}, []string{LblType, LblStatus, LblRule, LblOp})
+
+	KafkaWriterBatchDurationHist = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace: "kafka_client",
+		Subsystem: "writer",
+		Name:      "duration_microseconds",
+		Help:      "Histogram of Kafka Client Writer IO",
+		Buckets:   prometheus.ExponentialBuckets(10, 2, 20), // 10us ~ 5s
+	}, []string{LblType, LblRule, LblOp})
+)
+
+func init() {
+	prometheus.MustRegister(KafkaWriterBatchCounter)
+	prometheus.MustRegister(KafkaWriterErrCounter)
+	prometheus.MustRegister(KafkaWriterBatchDurationHist)
+}
