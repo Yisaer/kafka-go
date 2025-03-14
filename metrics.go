8121700cf22ccf83d69d5ace8204e721472d3a3a
Song Gao <disxiaofei@163.com>
2025-03-14 10:36:05 +0800

    use counter
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- metrics.go
+++ metrics.go
@@ -15,6 +15,33 @@ const (
 	LblBackOff            = "backoff"
 	LblMsg                = "msg"
 	LblBytes              = "bytes"
+	LblRange              = "range"
+)
+
+const (
+	LblRange1to10    = "<=10"
+	LblRange10to20   = "10-20"
+	LblRange20to40   = "20-40"
+	LblRange40to80   = "40-80"
+	LblRange80to160  = "80-160"
+	LblRange160to300 = "160-300"
+	LblRange300to500 = "300-500"
+	LblRange500toInf = ">500"
+)
+
+const (
+	Kb              = 1024
+	Mb              = 1024 * Kb
+	LblRange1Kb     = "<=1kb"
+	LblRange5Kb     = "1-5kb"
+	LblRange10Kb    = "5-10kb"
+	LblRange20Kb    = "10-20kb"
+	LblRange50Kb    = "20-50kb"
+	LblRange100Kb   = "50-100kb"
+	LblRange256Kb   = "100-256kb"
+	LblRange512Kb   = "256-512kb"
+	LblRangeMb      = "512kb-1Mb"
+	LblRangeMbToInf = ">1Mb"
 )
 
 var (
@@ -47,12 +74,7 @@ var (
 		Buckets:   prometheus.ExponentialBuckets(10, 2, 20), // 10us ~ 5s
 	}, []string{LblType, LblRule, LblOp})
 
-	KafkaWriterBatchGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Namespace: "kafka_client",
-		Subsystem: "writer_batch",
-		Name:      "gauge",
-		Help:      "Gauge of Kafka Client Writer IO",
-	}, []string{LblType, LblRule, LblOp})
+	KafkaWriterBatchRangeCounter = prometheus.NewCounterVec(prometheus.CounterOpts{}, []string{LblType, LblRange, LblRule, LblOp})
 )
 
 func init() {
@@ -60,5 +82,5 @@ func init() {
 	prometheus.MustRegister(KafkaWriterErrCounter)
 	prometheus.MustRegister(KafkaWriterBatchDurationHist)
 	prometheus.MustRegister(KafkaWriterBatchTotalBytes)
-	prometheus.MustRegister(KafkaWriterBatchGauge)
+	prometheus.MustRegister(KafkaWriterBatchRangeCounter)
 }
